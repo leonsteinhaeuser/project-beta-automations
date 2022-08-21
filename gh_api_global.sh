0febea26321f1c6e1b9cf3ca8de0939763194c6b
leonsteinhaeuser <leonsteinhaeuser@gmx.de>
2022-08-21 02:02:02 +0200

    enhancement: implemented GitHub graphql API refactor

--- gh_api_global.sh
+++ gh_api_global.sh
@@ -7,14 +7,14 @@
 function getItemID() {
     local project_id=$1
     local resource_id=$2
-    gh api graphql --header 'GraphQL-Features: projects_next_graphql' -f query='
-    mutation($project:ID!, $resource_id:ID!) {
-        addProjectNextItem(input: {projectId: $project, contentId: $resource_id}) {
-            projectNextItem {
+    gh api graphql -f query='
+    mutation($project:ID!, $pr:ID!) {
+        addProjectV2ItemById(input: {projectId: $project, contentId: $pr}) {
+            item {
                 id
             }
         }
-    }' -f project=$project_id -f resource_id=$resource_id --jq '.data.addProjectNextItem.projectNextItem.id' | sed -e "s+\"++g"
+    }' -f project=$project_id -f pr=$resource_id --jq '.data.addProjectV2ItemById.item.id'
 }
 
 # updateSingleSelectField updates the given item field with the defined value
@@ -28,27 +28,67 @@ function updateSingleSelectField() {
     local item_id=$2
     local field_id=$3
     local field_option=$4
-    echo $(gh api graphql --header 'GraphQL-Features: projects_next_graphql' -f query='
+
+    gh api graphql --header 'GraphQL-Features: projects_next_graphql' -f query='
     mutation (
         $project: ID!
         $item: ID!
         $fieldid: ID!
         $fieldOption: String!
     ) {
-        updateProjectNextItemField(
+        set_status: updateProjectV2ItemFieldValue(
             input: {
                 projectId: $project
                 itemId: $item
                 fieldId: $fieldid
-                value: $fieldOption
+                value: {
+                    singleSelectOptionId: $fieldOption
+                }
             }
         )
         {
-            projectNextItem {
+            projectV2Item {
                 id
             }
         }
-    }' -f project=$project_id -f item=$item_id -f fieldid=$field_id -f fieldOption=$field_option | sed -e "s+\"++g")
+    }' -f project=$project_id -f item=$item_id -f fieldid=$field_id -f fieldOption=$field_option | sed -e "s+\"++g"
+}
+
+# updateIterationField updates the given item field with the defined value
+# Required arguments:
+#   1: project id
+#   2: project item id
+#   3: field id
+#   4: field option string (id as string)
+function updateIterationField() {
+    local project_id=$1
+    local item_id=$2
+    local field_id=$3
+    local field_option=$4
+
+    gh api graphql --header 'GraphQL-Features: projects_next_graphql' -f query='
+    mutation (
+        $project: ID!
+        $item: ID!
+        $fieldid: ID!
+        $fieldOption: String!
+    ) {
+        set_status: updateProjectV2ItemFieldValue(
+            input: {
+                projectId: $project
+                itemId: $item
+                fieldId: $fieldid
+                value: {
+                    iterationId: $fieldOption
+                }
+            }
+        )
+        {
+            projectV2Item {
+                id
+            }
+        }
+    }' -f project=$project_id -f item=$item_id -f fieldid=$field_id -f fieldOption=$field_option | sed -e "s+\"++g"
 }
 
 # updateTextField updates the given item field with the defined value
@@ -62,25 +102,26 @@ function updateTextField() {
     local ITEM_ID=$2
     local FIELD_ID=$3
     local FIELD_VALUE=$4
-    echo $(gh api graphql --header 'GraphQL-Features: projects_next_graphql' -f query='
+    gh api graphql -f query='
     mutation (
         $project: ID!
         $item: ID!
         $fieldid: ID!
         $fieldValue: String!
     ) {
-        updateProjectNextItemField(
+        updateProjectV2ItemFieldValue(
             input: {
                 projectId: $project
                 itemId: $item
                 fieldId: $fieldid
-                value: $fieldValue
+                value: {
+                    text: $fieldValue
+                }
             }
-        )
-        {
-            projectNextItem {
+        ) {
+            projectV2Item {
                 id
             }
         }
-    }' -f project=$PROJECT_ID -f item=$ITEM_ID -f fieldid=$FIELD_ID -f fieldValue="$FIELD_VALUE" | sed -e "s+\"++g")
+    }' -f project=$PROJECT_ID -f item=$ITEM_ID -f fieldid=$FIELD_ID -f fieldValue="$FIELD_VALUE" | sed -e "s+\"++g"
 }
