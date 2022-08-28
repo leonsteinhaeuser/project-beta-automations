54b97a08b8ed36090d0dca6b7cdb3b29a0e6d248
leonsteinhaeuser <leonsteinhaeuser@gmx.de>
2022-08-28 22:47:54 +0200

    enhancement: improved organization lib

--- gh_api_lib_organization.sh
+++ gh_api_lib_organization.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-TMP_STORE_LOCATION=/tmp/api_response.json
-
 # getOrganizationProjectID queries the github api for the specific project uuid
-#   1: organization
+#   1: organization (string)
 #   2: project id (number)
 function getOrganizationProjectID() {
     local ORGANIZATION=$1
     local PROJECT_NUMBER=$2
     gh api graphql --header 'GraphQL-Features: projects_next_graphql' -f query='
-    query($organization: String!, $number: Int!) {
-      organization(login: $organization){
+    query($org: String!, $number: Int!) {
+      organization(login: $org){
         projectV2(number: $number) {
           id
         }
       }
-    }' -f organization=$ORGANIZATION -F number=$PROJECT_NUMBER > $TMP_STORE_LOCATION
+    }' -f org=$ORGANIZATION -F number=$PROJECT_NUMBER | jq .data.organization.projectV2.id | sed -e 's+"++g'
 }
 
 # getOrganizationProjectFields queries the github api for the specific project fields
@@ -56,43 +54,4 @@ function getOrganizationProjectFields() {
         }
       }
     }' -f projectId=$PROJECT_ID | jq .data.node.fields > $TMP_STORE_LOCATION
-}
-
-# extractOrganizationFieldID returns the field id
-#  1: field name
-function extractOrganizationFieldID() {
-    local fieldName=$(echo $1 | sed -e "s+\"++g") # remove quotes
-    jq -r ".nodes[] | select(.name == \"$fieldName\").id" $TMP_STORE_LOCATION
-}
-
-# extractOrganizationFieldNodeIterationSettingValue returns the field node setting value id
-#   1: field name
-#   2: select value
-#
-# NOTE: If the value is @current or @next, we check the the array of iterations and return the current or next iteration id.
-function extractOrganizationFieldNodeIterationSettingValue() {
-    local field_name=$(echo $1 | sed -e "s+\"++g") # remove quotes
-    local select_value=$(echo $2 | sed -e "s+\"++g") # remove quotes
-
-    iterations_for_field=$(jq ".nodes[] | select(.name==\"$field_name\").configuration.iterations" $TMP_STORE_LOCATION)
-    dates=$(echo $iterations_for_field | jq -r '.[] | .startDate' | sort)
-    STRINGTEST=(${dates[@]})
-    if [ "$select_value" == "@current" ]; then
-        CURRENT_ITERATION=${STRINGTEST[0]}
-        echo -e $iterations_for_field | jq -r '.[] | select(.startDate == "'$CURRENT_ITERATION'").id'
-    elif [ "$select_value" == "@next" ]; then
-        NEXT_ITERATION=${STRINGTEST[1]}
-        echo -e $iterations_for_field | jq -r '.[] | select(.startDate == "'$NEXT_ITERATION'").id'
-    else
-        echo -e $iterations_for_field | jq -r ".[] | select(.title==\"$select_value\") | .id"
-    fi
-}
-
-# extractOrganizationFieldNodeSelectSettingValue returns the field node setting value id
-#   1: field name
-#   2: select value
-function extractOrganizationFieldNodeSelectSettingValue() {
-    local fieldName=$(echo $1 | sed -e "s+\"++g") # remove quotes
-    local selectValue=$(echo $2 | sed -e "s+\"++g") # remove quotes
-    jq ".nodes[] | select(.name==\"Status\").options[] | select(.name==\"Done\").id" $TMP_STORE_LOCATION | sed -e "s+\"++g"
 }
