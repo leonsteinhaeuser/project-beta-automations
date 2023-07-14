53acae6f1e946f09286ef0ee570dcf1608f79186
Petros Andreou <p.andreou@unicaf.org>
2023-07-14 12:48:37 +0300

    add support for arm-based runners

--- deps.sh
+++ deps.sh
@@ -9,6 +9,20 @@ fi
 version=${GH_CLI_VERSION:-2.8.0}
 echo "Installing gh cli in version: $version"
 
-wget https://github.com/cli/cli/releases/download/v${version}/gh_${version}_linux_amd64.tar.gz
-tar -xvf gh_${version}_linux_amd64.tar.gz
-sudo cp $(pwd)/gh_${version}_linux_amd64/bin/gh /usr/local/bin/
+machine=$(uname -m)
+case $machine in
+    aarch64)
+        arch="arm64"
+        ;;
+    x86_64)
+        arch="amd64"
+        ;;
+    *)
+        echo "Unsupported architecture: $machine"
+        exit 1
+        ;;
+esac
+
+wget https://github.com/cli/cli/releases/download/v${version}/gh_${version}_linux_${arch}.tar.gz
+tar -xvf gh_${version}_linux_${arch}.tar.gz
+sudo cp $(pwd)/gh_${version}_linux_${arch}/bin/gh /usr/local/bin/
